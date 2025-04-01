887e35913f86927a3253da63c27bf36a7c9e37af
zahid4kh <halilzahid@gmail.com>
2025-04-01 15:55:09 +0200

    upd neobutton: made the width dynamic and updated default colors

--- neobrutal/src/main/java/zahid/neobrutal/buttons/NeoButton.kt
+++ neobrutal/src/main/java/zahid/neobrutal/buttons/NeoButton.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,6 +25,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 /**
  * A button component in NeoBrutal style.
@@ -34,59 +38,61 @@ import androidx.compose.ui.unit.dp
  * @param text The text displayed on the button.
  * @param onClick The callback to be invoked when the button is clicked.
  * @param enabled Controls the enabled state of the button. When `false`, the button will be grayed out and not clickable.
- * @param isPressed Indicates whether the button is currently pressed. This affects the 3D effect.
  * @param backgroundColor The background color of the button.
  * @param textColor The color of the button text.
  * @param shadowColor The color of the 3D shadow effect.
+ * @param shadowOffset How far the shadow is offset from the button.
  * @param shape The shape of the button (defaults to rectangular).
- * @param width The width of the button.
- * @param height The height of the button.
  */
 @Composable
 fun NeoButton(
-    modifier: Modifier = Modifier,
     text: String,
     onClick: () -> Unit,
-    isPressed: Boolean = false,
+    modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    backgroundColor: Color = Color(0xFFFF5470),
-    textColor: Color = Color.White,
-    shadowColor: Color = Color.Black,
-    shape: Shape = RectangleShape,
-    width: Dp = 200.dp,
-    height: Dp = 100.dp
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
+    textColor: Color = MaterialTheme.colorScheme.onBackground,
+    shadowColor: Color = MaterialTheme.colorScheme.onBackground,
+    shadowOffset: Dp = 6.dp,
+    shape: Shape = RectangleShape
 ) {
+    var isPressed by remember { mutableStateOf(false) }
+
+    LaunchedEffect(isPressed) {
+        if (isPressed) {
+            onClick()
+            delay(40)
+            isPressed = false
+        }
+    }
+
     Box(
         modifier = modifier
-            .clickable(
-                enabled = enabled,
-                onClick = {
-                    onClick()
-                }
-            )
-            .size(width = width, height = height)
-            .padding(bottom = 8.dp, end = 8.dp)
+            .wrapContentSize()
+            .padding(bottom = shadowOffset, end = shadowOffset)
     ) {
         Box(
             modifier = Modifier
-                .offset(x = 8.dp, y = 8.dp)
+                .matchParentSize()
+                .offset(x = shadowOffset, y = shadowOffset)
                 .background(shadowColor, shape)
         )
         Box(
             modifier = Modifier
-                .matchParentSize()
+                .wrapContentSize()
                 .offset(
-                    x = if (isPressed) 4.dp else 0.dp,
-                    y = if (isPressed) 4.dp else 0.dp
+                    x = if (isPressed) shadowOffset / 2 else 0.dp,
+                    y = if (isPressed) shadowOffset / 2 else 0.dp
                 )
+                .clickable(enabled = enabled) {
+                    isPressed = true
+                }
                 .background(backgroundColor, shape)
                 .drawBehind {
                     drawRect(
                         color = shadowColor,
                         size = size,
-                        style = Stroke(
-                            width = 2f
-                        )
+                        style = Stroke(width = 5f)
                     )
                 },
             contentAlignment = Alignment.Center
@@ -95,9 +101,10 @@ fun NeoButton(
                 text = text,
                 color = textColor,
                 style = TextStyle(
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
                 ),
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)
             )
         }
     }
